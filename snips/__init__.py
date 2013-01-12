c430fcb6ed1b19eddfbfd27d1cfcc84d0c4ea220
fission6 <fission6@fission6.(none)>
2013-01-12 13:08:25 -0500

    folder cleanup

--- snips/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Need to dynamically load all python modules
-in the snips/ folder.
-"""
-
-from pt_snip import PTSnip
-from twitter_snip import TwitterSnip
-from slashdot_snip import SlashdotSnip
-from snip import registered_snips
